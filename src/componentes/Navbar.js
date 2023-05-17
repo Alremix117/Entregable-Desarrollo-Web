c6da082f39cfa51f7698030db2b38f05f5069c79
Alremix117 <alexander312645@gmail.com>
2023-05-17 18:26:40 -0500

    Actualizar package.json y Navbar.js

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -3,14 +3,15 @@ import Logo from "../Assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs"
 import { HiOutlineBars3 } from "react-icons/hi2"
 
-import {
+/*import {
     Box,
     Drawer,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
-} from "@mui/material";
+} from "@mui/material";*/
+
 import HomeIcon from "@mui/icons-material/Home"
 import InfoIcon from "@mui/icons-material/Info"
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
